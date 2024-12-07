f4853eb65c80406428529a1330beed8537dea15c
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-12-07 21:53:13 +0530

    store-persistance

---
+++ src/components/EmptyStateCard.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { FaShoppingBag } from "react-icons/fa"
+import { Link } from "react-router"
+
+const EmptyStateCard = ({
+  icon: Icon = FaShoppingBag,
+  heading,
+  subheading,
+  buttonLabel,
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-white rounded-md shadow-[0_2px_12px_-3px_rgba(6,81,237,0.3)] p-8 space-y-4 max-w-md mx-auto mt-20">
+      {/* Icon */}
+      <div className="p-4 bg-blue-50 text-blue-600 rounded-full">
+        <Icon size={48} />
+      </div>
+
+      {/* Text Section */}
+      <div className="text-center">
+        <h2 className="text-xl font-bold text-gray-800">{heading}</h2>
+        <p className="text-sm text-gray-500">{subheading}</p>
+      </div>
+
+      <Link to="/">
+        <button className="px-6 py-2 text-sm font-semibold text-white bg-gray-800 rounded-md hover:bg-gray-900">
+          {buttonLabel}
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+export default EmptyStateCard
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import "./index.css"
 import App from "./App.jsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Provider } from "react-redux"
-import { store } from "./store/store.js"
+import { store, persistor } from "./store/store.js"
+import { PersistGate } from "redux-persist/integration/react"
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </QueryClientProvider>
 )
